fb8e88c3483999c594775f5204ca1fbbb046444c
starchenko <redux1234567@mail.ru>
2021-03-06 02:50:45 +0600

    Ended with poll result controller

--- backend/src/main/java/com/lolspolls/app/controllers/ElementController.java
+++ backend/src/main/java/com/lolspolls/app/controllers/ElementController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,12 +33,12 @@ public class ElementController {
     }
 
     @PostMapping
-    public ElementDto createElement(ElementCreateDto element) {
+    public ElementDto createElement(@RequestBody ElementCreateDto element) {
         return this.elementsCrudService.createElement(element);
     }
 
     @PutMapping(path = "/update")
-    public ElementDto updateElement(ElementUpdateDto element) {
+    public ElementDto updateElement(@RequestBody ElementUpdateDto element) {
         return this.elementsCrudService.updateElement(element);
     }
 
--- backend/src/main/java/com/lolspolls/app/controllers/PollResultController.java
+++ backend/src/main/java/com/lolspolls/app/controllers/PollResultController.java
@@ -1,15 +1,36 @@
 package com.lolspolls.app.controllers;
 
+import java.util.List;
+
+import com.lolspolls.app.dto.create.PollResultCreateDto;
+import com.lolspolls.app.dto.read.PollResultDto;
 import com.lolspolls.app.services.crud.PollResultsCrudService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/element")
+@RequestMapping("/result")
 @RequiredArgsConstructor
 public class PollResultController {
 
     private final PollResultsCrudService pollResultsCrudService;
+
+    //
+    // API
+    //
+
+    @GetMapping
+    public List<PollResultDto> getResults() {
+        return this.pollResultsCrudService.getPollResults();
+    }
+
+    @PostMapping
+    public PollResultDto createResult(@RequestBody PollResultCreateDto result) {
+        return this.pollResultsCrudService.createPollResult(result);
+    }
 
 }
